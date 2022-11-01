1ce01ace73dad185f0f5d0fd34e0ec31bb16fe72
Heath Brown <heathd.brown@gmail.com>
2022-11-01 11:40:52 -0500

    black format

--- pscanner/__init__.py
+++ pscanner/__init__.py
@@ -12,12 +12,15 @@ GREEN = Fore.GREEN
 RESET = Fore.RESET
 GRAY = Fore.LIGHTBLACK_EX
 
+
 def is_host_alive(host: str) -> bool:
     if ping(host, timeout=1, count=1).success():
         return True
     else:
         print(f"{GRAY}{host:15} is not alive {RESET}")
-        return False    
+        return False
+
+
 def is_port_open(host: str, port: int) -> bool:
     try:
         s = socket.socket()
@@ -31,14 +34,16 @@ def is_port_open(host: str, port: int) -> bool:
     finally:
         s.close()
 
+
 def is_subnet(ip: str) -> bool:
-    if '/' in ip:
+    if "/" in ip:
         return True
     else:
         return False
 
+
 def hosts_in_subnet(network: str) -> List:
     if not is_subnet:
         print(f"{network} is not a network")
-    
+
     return list(ipaddress.ip_network(network).hosts())
--- pscanner/cli/__init__.py
+++ pscanner/cli/__init__.py
@@ -6,16 +6,19 @@ from pscanner import is_port_open, is_subnet, hosts_in_subnet, is_host_alive
 from ..__about__ import __version__
 
 
-@click.group(context_settings={'help_option_names': ['-h', '--help']}, invoke_without_command=True)
-@click.version_option(version=__version__, prog_name='pscanner')
-@click.argument('host')
-@click.argument('port')
+@click.group(
+    context_settings={"help_option_names": ["-h", "--help"]},
+    invoke_without_command=True,
+)
+@click.version_option(version=__version__, prog_name="pscanner")
+@click.argument("host")
+@click.argument("port")
 @click.pass_context
 def pscanner(ctx: click.Context, host, port):
     if is_subnet(host):
         for ip in hosts_in_subnet(host):
             if is_host_alive(str(ip)):
                 is_port_open(str(ip), port)
-    else:       
-        if is_host_alive(host):     
+    else:
+        if is_host_alive(host):
             is_port_open(host, port)
